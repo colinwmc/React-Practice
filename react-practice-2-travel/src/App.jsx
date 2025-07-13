652025ac80cbd067db387530888390f93f2868d6
Colin McGregor <100391145+colinwmc@users.noreply.github.com>
2025-07-13 19:09:47 +0200

    starting 2

---
+++ react-practice-2-travel/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/header"
+import Entry from "./components/Entry"
+
+export default function App(){
+  return (
+    <>
+    <Header></Header>
+    <Entry></Entry>
+    </>
+  )
+} 
+
---
+++ react-practice-2-travel/src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header(){
+    return (
+        <header className="header">
+            <div className="logo-and-title">
+                <img className="logo" src="src\assets\Globe.png" alt="globe" />
+            <span className="header-title">my travel journal.</span>
+            </div>
+        </header>
+    )
+}
