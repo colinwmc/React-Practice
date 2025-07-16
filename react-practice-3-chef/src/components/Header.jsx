8646bb916c8d37109a7cda4634ba0b0a9252b21e
Colin McGregor <100391145+colinwmc@users.noreply.github.com>
2025-07-16 16:57:51 +0200

    starting 3

---
+++ react-practice-3-chef/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import chefClaudeLogo from "../assets/Chef-Claude-Icon.png"
+
+export default function Header() {
+    return (
+        <header className='header'>
+            <img className="header-logo" src={chefClaudeLogo} alt="chef icon" />
+            <h1 className="header-title">Chef Claude</h1>
+        </header>
+    )
+}
