0505b6c5ae9dfb51622e0aeb5bd2c70f9eb04543
Colin McGregor <100391145+colinwmc@users.noreply.github.com>
2025-07-18 22:50:38 +0200

    state

---
+++ react-practice-3-chef/src/components/Main.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+
+export default function Main() {
+
+    const [ingredients, setIngredients] = useState([]);
+
+    const ingredientsListItems = ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
+
+    function submitForm(event) {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+        const newIngredient = formData.get("ingredient");
+        setIngredients(prevIngredients => [...prevIngredients, newIngredient])
+    }
+
+    return (
+        <main>
+            <form
+                className="add-ingredient-form"
+                onSubmit={submitForm}>
+                <input
+                    aria-label="Add ingedient"
+                    type="text"
+                    placeholder="e.g. oregano"
+                    name="ingredient"
+                />
+                <button>Add ingredient</button>
+            </form>
+            <ul>
+                {ingredientsListItems}
+            </ul>
+        </main>
+    )
+}
